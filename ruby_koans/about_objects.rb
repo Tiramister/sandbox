1f8d8033562cee57dc9042ccdeb02068e989e58d
Tiramister <nh111424@gmail.com>
2024-03-09 17:57:05 +0900

    feat(ruby_koans): about_objects

--- ruby_koans/about_objects.rb
+++ ruby_koans/about_objects.rb
@@ -1,50 +1,83 @@
-require File.expand_path(File.dirname(__FILE__) + '/neo')
+# frozen_string_literal: true
+
+require File.expand_path("#{File.dirname(__FILE__)}/neo")
 
 class AboutObjects < Neo::Koan
   def test_everything_is_an_object
-    assert_equal __, 1.is_a?(Object)
-    assert_equal __, 1.5.is_a?(Object)
-    assert_equal __, "string".is_a?(Object)
-    assert_equal __, nil.is_a?(Object)
-    assert_equal __, Object.is_a?(Object)
+    assert_equal true, 1.is_a?(Object)
+    assert_equal true, 1.5.is_a?(Object)
+    assert_equal true, 'string'.is_a?(Object)
+    assert_equal true, nil.is_a?(Object)
+    assert_equal true, Object.is_a?(Object)
   end
 
   def test_objects_can_be_converted_to_strings
-    assert_equal __, 123.to_s
-    assert_equal __, nil.to_s
+    assert_equal '123', 123.to_s
+
+    # これ紛らわしいので注意
+    assert_equal '', nil.to_s
   end
 
   def test_objects_can_be_inspected
-    assert_equal __, 123.inspect
-    assert_equal __, nil.inspect
+    # こっちは人間が読める形式に変換する
+    assert_equal '123', 123.inspect
+    assert_equal 'nil', nil.inspect
   end
 
   def test_every_object_has_an_id
     obj = Object.new
-    assert_equal __, obj.object_id.class
+    assert_equal Integer, obj.object_id.class
   end
 
   def test_every_object_has_different_id
     obj = Object.new
     another_obj = Object.new
-    assert_equal __, obj.object_id != another_obj.object_id
+    assert_equal true, obj.object_id != another_obj.object_id
   end
 
   def test_small_integers_have_fixed_ids
-    assert_equal __, 0.object_id
-    assert_equal __, 1.object_id
-    assert_equal __, 2.object_id
-    assert_equal __, 100.object_id
+    assert_equal 1, 0.object_id
+    assert_equal 3, 1.object_id
+    assert_equal 5, 2.object_id
+    assert_equal 201, 100.object_id
 
     # THINK ABOUT IT:
     # What pattern do the object IDs for small integers follow?
+
+    # (0..20).each do |id|
+    #   puts "#{id} -> #{ObjectSpace._id2ref(id).inspect}"
+    # rescue StandardError => e
+    #   puts e
+    # end
+    #
+    # 0 -> false
+    # 1 -> 0
+    # 2 -> 2.0
+    # 3 -> 1
+    # 4 -> nil
+    # 5 -> 2
+    # 6 -> -2.0
+    # 7 -> 3
+    # 0x0000000000000010 is not symbol id value
+    # 9 -> 4
+    # 10 -> 2.0000000000000004
+    # 11 -> 5
+    # "12" is recycled object
+    # 13 -> 6
+    # 14 -> -2.0000000000000004
+    # 15 -> 7
+    # "16" is recycled object
+    # 17 -> 8
+    # 18 -> 2.000000000000001
+    # 19 -> 9
+    # 20 -> true
   end
 
   def test_clone_creates_a_different_object
     obj = Object.new
     copy = obj.clone
 
-    assert_equal __, obj           != copy
-    assert_equal __, obj.object_id != copy.object_id
+    assert_equal true, obj != copy
+    assert_equal true, obj.object_id != copy.object_id
   end
 end

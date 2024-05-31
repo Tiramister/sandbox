5a59b601d339abe3fcdd24c599592d8002c35fdc
Tiramister <contact@tiramister.net>
2024-06-01 08:38:52 +0900

    style(compose): ktlint を導入

--- android-tutorial/AndroidBasicsCompose/build.gradle.kts
+++ android-tutorial/AndroidBasicsCompose/build.gradle.kts
@@ -2,4 +2,9 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 }

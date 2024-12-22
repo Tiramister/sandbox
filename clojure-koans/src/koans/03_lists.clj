498128622c4152057028f5861006bd4f5f115004
Tiramister <contact@tiramister.net>
2024-12-23 00:06:18 +0900

    feat(clojure-koans): 03

--- clojure-koans/src/koans/03_lists.clj
+++ clojure-koans/src/koans/03_lists.clj
@@ -3,43 +3,52 @@
 
 (meditations
  "Lists can be expressed by function or a quoted form"
- (= '(__ __ __ __ __) (list 1 2 3 4 5))
+ (= '(1 2 3 4 5) (list 1 2 3 4 5))
 
  "They are Clojure seqs (sequences), so they allow access to the first"
- (= __ (first '(1 2 3 4 5)))
+ (= 1 (first '(1 2 3 4 5)))
 
  "As well as the rest"
- (= __ (rest '(1 2 3 4 5)))
+ (= '(2 3 4 5) (rest '(1 2 3 4 5)))
 
  "Count your blessings"
- (= __ (count '(dracula dooku chocula)))
+ ;; リストの中身は 3 つとも変数っぽいが、どこかで定義されているのか？
+ (= 3 (count '(dracula dooku chocula)))
 
  "Before they are gone"
- (= __ (count '()))
+ (= 0 (count '()))
 
  "The rest, when nothing is left, is empty"
- (= __ (rest '(100)))
+ (= '() (rest '(100)))
 
  "Construction by adding an element to the front is easy"
- (= __ (cons :a '(:b :c :d :e)))
+ (= '(:a :b :c :d :e) (cons :a '(:b :c :d :e)))
 
  "Conjoining an element to a list isn't hard either"
- (= __ (conj '(:a :b :c :d) :e))
+ ;; 挿入される箇所はデータ構造によって異なる
+ ;; リストの場合は先頭 (cons と等価)
+ (= '(:e :a :b :c :d) (conj '(:a :b :c :d) :e))
 
  "You can use a list like a stack to get the first element"
- (= __ (peek '(:a :b :c :d :e)))
+ ;; リストの場合は first と等価だが、ベクタの場合は last と等価
+ (= :a (peek '(:a :b :c :d :e)))
 
  "Or the others"
- (= __ (pop '(:a :b :c :d :e)))
+ (= '(:b :c :d :e) (pop '(:a :b :c :d :e)))
 
  "But watch out if you try to pop nothing"
- (= __ (try
-         (pop '())
-         (catch IllegalStateException e
-           "No dice!")))
+ (= "No dice!" (try
+                 (pop '())
+                 (catch IllegalStateException e
+                   "No dice!")))
 
  "The rest of nothing isn't so strict"
- (= __ (try
-         (rest '())
-         (catch IllegalStateException e
-           "No dice!"))))
+ (= '() (try
+          (rest '())
+          (catch IllegalStateException e
+            "No dice!"))))
+
+;; まとめ
+;; リストのリテラルは '() で囲む
+;; first で先頭 (car)、rest で先頭以外の要素 (cdr) を取得する
+;; conj (挿入)、peek (1 件取得)、pop (1 件削除) はデータ構造によって挙動が異なる

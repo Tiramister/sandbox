354049393ab93148ec3b063429f9ace744347f09
Tiramister <contact@tiramister.net>
2025-01-05 20:09:33 +0900

    feat(clojure-koans): 20

--- clojure-koans/src/koans/20_java_interop.clj
+++ clojure-koans/src/koans/20_java_interop.clj
@@ -3,17 +3,24 @@
 
 (meditations
  "You may have done more with Java than you know"
- (= __ (class "warfare")) ; hint: try typing (javadoc "warfare") in the REPL
+ (= java.lang.String (class "warfare")) ; hint: try typing (javadoc "warfare") in the REPL
 
  "The dot signifies easy and direct Java interoperation"
- (= __ (.toUpperCase "select * from"))
+ (= "SELECT * FROM" (.toUpperCase "select * from"))
 
  "But instance method calls are very different from normal functions"
- (= ["SELECT" "FROM" "WHERE"] (map ___ ["select" "from" "where"]))
+ (= ["SELECT" "FROM" "WHERE"] (map #(.toUpperCase %) ["select" "from" "where"]))
 
  "Constructing might be harder than breaking"
- (= 10 (let [latch (java.util.concurrent.CountDownLatch. __)]
+ (= 10 (let [latch (java.util.concurrent.CountDownLatch. 10)]
          (.getCount latch)))
 
  "Static methods are slashing prices!"
- (== __ (Math/pow 2 10)))
+ (== 1024 (Math/pow 2 10)))
+
+;; まとめ
+;; Java の機能も普通に使える
+;; (.<field> <instance>) でフィールドにアクセスできる
+;; ただし .<field> は関数ではなくマクロなので、関数としては使えない
+;; (<class>. <args) でコンストラクタを呼び出し、インスタンスを生成する
+;; 静的メソッドは <class>/<method> で呼び出せる
